cbca90a150d7586fb4feec0bad5fe5f56c5c87e8
poteto <naonyan1135@gmail.com>
2025-01-15 02:35:18 +0900

    コードスタイルの改善と不要な依存関係の削除

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -10,16 +10,15 @@ type DataType = {
 
 export default function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params); // params をアンラップ
-  const { data, isLoading, isError } = useData<DataType>(`api/posts/${id}`);
+  const { data, isLoading, isError } = useData<DataType>(`api/posts/${id}`)
 
-  if (isLoading) return <div className="m-auto text-center text-4xl font-bold">Loading...</div>;
-  if (isError) return <div className="m-auto text-center text-4xl font-bold">Error</div>;
+  if (isLoading) return <div className="m-auto text-center text-4xl font-bold">Loading...</div>
+  if (isError) return <div className="m-auto text-center text-4xl font-bold">Error</div>
   return (<>
   <div>
     <p className="m-auto text-center text-4xl font-bold">
     id:{id} message:{data!.message} content:{data!.content}
     </p>
     </div>
-  
-  </>);
+  </>)
 }
--- app/utils/client.ts
+++ app/utils/client.ts
@@ -1,16 +1,16 @@
-import useSWR from "swr";
+import useSWR from "swr"
 
 const fetcher = async (url: string) => {
-  const res = await fetch(url);
-  return res.json();
-};
+  const res = await fetch(url)
+  return res.json()
+}
 
 export const useData = <T>(endpoint: string) => {
-  const { data, error } = useSWR(`http://localhost:8080/${endpoint}`, fetcher);
+  const { data, error } = useSWR(`http://localhost:8080/${endpoint}`, fetcher)
 
   return {
     data: data as T, //* as T は型アサーションでこれは型 T であると TypeScript に伝える
     isLoading: !error && !data,
     isError: error,
-  };
-};
+  }
+}
